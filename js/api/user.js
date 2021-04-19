3abfd074b2bd6fa62449a548ebe604ca45eaeac8
Liana <lianam@connectto.com>
2021-04-19 17:34:45 +0400

    friendrequest

--- js/api/user.js
+++ js/api/user.js
@@ -28,3 +28,35 @@ export const findUsers = async (name) => {
     }
     return response.json();
 }
+export const friendRequest = async (to) => {
+    const token = window.localStorage.getItem('token');
+    const response = await fetch('http://localhost:2021/users/friend-request', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': token
+        },
+        body: JSON.stringify({to})
+    });
+    if (response.status === 401) {
+        window.localStorage.removeItem('token');
+        window.location.href = '/login.html'
+    }
+    return response.json();
+}
+
+export const getFriendRequests = async () => {
+    const token = window.localStorage.getItem('token');
+    const response = await fetch('http://localhost:2021/users/friend-request', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': token
+        }
+    });
+    if (response.status === 401) {
+        window.localStorage.removeItem('token');
+        window.location.href = '/login.html'
+    }
+    return response.json();
+}
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,18 +1,37 @@
-import {getCurrentUserData,findUsers
+import {getCurrentUserData,findUsers,friendRequest, getFriendRequests
 
 } from '../api/user.js';
 
 window.addEventListener('load', async () => {
     document.getElementById('log-out').addEventListener('click', function (){
         window.localStorage.removeItem('token');
-        window.location.href = '/login.html'
+        window.location.replace('localhost/frontend/login.html');
     });
     getCurrentUserData().then((response)=>{
         if(response.success === true){
             document.getElementById('user-name').innerText = response.data.name;
         }
     });
+    getFriendRequests().then((response) => {
+        const tbody = document.querySelector('#requests tbody');
+        const acceptClicks = () => {
 
+        }
+        if (response.data && Object.keys(response.data).length > 0) {
+            let innerHTML = '';
+            console.log(response.data);
+            response.data.map(user => {
+                innerHTML += `<tr>
+                            <td>${user.name}</td>
+                            <td>
+                                <button class="friend-request" user-id="${user._id}" >Accept Request</button>
+                            </td>
+                        </tr>`;
+            });
+            tbody.innerHTML = innerHTML;
+            acceptClicks();
+        }
+    });
 
     const search = document.querySelector('input[name="find-friend"]');
     const tbody = document.querySelector('#users tbody');
@@ -28,21 +47,17 @@ window.addEventListener('load', async () => {
     }
     search.addEventListener('input', async () => {
         if (search.value.length > 0) {
-            const response = await findUsers(search.value); console.log(response);
+            const response = await findUsers(search.value);
             if (response.data) {
-                let innerHTML = '';
+                let innerHTML = ''; 
                 response.data.map(user => {
                     innerHTML += `<tr>
                             <td>${user.name}</td>
                             <td>
-                                ${
-                        currentUser.friends.includes(user._id) ? 'Friends' :
-                            currentUser.sentFriendRequests.includes(user._id) ? 'request sent':`
-                <button class="friend-request" user-id="${user._id}">Friend Request</button>`
-                                    }
-                                            </td>
-                                        </tr>`;
-                });
+                                <button class="friend-request" user-id="${user._id}" >Friend Request</button>
+                            </td>
+                        </tr>`;
+                }); 
                 tbody.innerHTML = innerHTML;
                 requestClicks();
             }
